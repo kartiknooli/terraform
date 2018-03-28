054e733443bddd989e355ffefc71240dd73574ba
Kartik Nooli <kartikn@kartikn-mbp.3amlabs.net>
2018-03-28 15:34:22 -0700

    adding new folder and first tf files

---
+++ base/variables.tf
@@ -0,0 +1,12 @@
+variable "access_key" {
+	description="Your AWS access key."
+}
+
+variable "secret_key" {
+	description="Your AWS secret key"
+}
+
+variable "region" {
+	description="AWS Region"
+	default="us-east-1"
+}
